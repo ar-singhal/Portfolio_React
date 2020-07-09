28d3cd227343c36728b38d64b164d25613b3fac2
ar-singhal <aryansinghal123457@gmail.com>
2020-07-10 00:12:22 +0530

    Updates

---
+++ precache-manifest.5594fe0950e4df0d6f66cfc83b103c55.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82e7b1947ea6500dea2a7e58c5fd2c4",
+    "url": "/aryan_portfolio_react/index.html"
+  },
+  {
+    "revision": "23b5d3a5b8a1f9df5092",
+    "url": "/aryan_portfolio_react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "56d7fdb78d6d40ec410c",
+    "url": "/aryan_portfolio_react/static/css/main.fa2a69da.chunk.css"
+  },
+  {
+    "revision": "23b5d3a5b8a1f9df5092",
+    "url": "/aryan_portfolio_react/static/js/2.15c37109.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/aryan_portfolio_react/static/js/2.15c37109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d7fdb78d6d40ec410c",
+    "url": "/aryan_portfolio_react/static/js/main.21dcb7d2.chunk.js"
+  },
+  {
+    "revision": "759625f905734e801ad7",
+    "url": "/aryan_portfolio_react/static/js/runtime-main.bab6600e.js"
+  },
+  {
+    "revision": "7987fbca77601bb82ff127e2aee59d67",
+    "url": "/aryan_portfolio_react/static/media/ar.7987fbca.jpeg"
+  },
+  {
+    "revision": "2db97d69ba44f8da4663584e8197143f",
+    "url": "/aryan_portfolio_react/static/media/css.2db97d69.svg"
+  },
+  {
+    "revision": "ce6e5ba07db5030872f1c4934b16a166",
+    "url": "/aryan_portfolio_react/static/media/html.ce6e5ba0.svg"
+  }
+]);
